80a5d6305ce83b65273029e666f5ddf660057322
여지윤 <81175074+yeojiyoon@users.noreply.github.com>
2024-05-04 01:43:33 +0900

    ADD POST API

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -2,8 +2,13 @@ package com.group.libraryapp.controller.calculator;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
+
 
 
 @RestController //주어진 class를 controller로 등록 (controller: API입구)
@@ -16,5 +21,11 @@ public class CalculatorController {
     ) { //쿼리
         return num1 + num2; //반환 결과
     }
+
+    @PostMapping("/multiply")
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNumber1() * request.getNumber2();
+    }
+    
     
 }
