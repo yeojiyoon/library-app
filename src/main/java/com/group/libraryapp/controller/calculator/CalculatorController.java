6d88e8578a6166c819c0a595187b15727f93aee4
여지윤 <81175074+yeojiyoon@users.noreply.github.com>
2024-05-04 01:14:08 +0900

    FIRST COMMIT

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,8 +1,20 @@
 package com.group.libraryapp.controller.calculator;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@RestController //주어진 class를 controller로 등록 (controller: API입구)
 public class CalculatorController {
 
+    @GetMapping("/add") //HTTP Method / HTTP Path
+    public int addTwoNumbers(
+        @RequestParam("num1") int num1,
+        @RequestParam("num2") int num2 
+    ) { //쿼리
+        return num1 + num2; //반환 결과
+    }
+    
 }
---
+++ src/main/java/com/group/libraryapp/dto/calculator/request/CalculatorAddRequest.java
@@ -0,0 +1,10 @@
+package com.group.libraryapp.dto.calculator.request;
+
+public class CalculatorAddRequest {
+
+    //private final int numb1;
+    //private final int num2;
+
+    
+
+}
