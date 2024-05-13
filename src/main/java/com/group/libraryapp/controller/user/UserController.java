14d9ba4b4d070b5ea2372d2a782a602d5f640077
여지윤 <81175074+yeojiyoon@users.noreply.github.com>
2024-05-13 15:10:00 +0900

    API FINISH

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,14 +1,17 @@
 package com.group.libraryapp.controller.user;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+
+
 
 
 @RestController
@@ -16,9 +19,22 @@ public class UserController {
 
     private final List<User> users = new ArrayList<>();
     
-    @PostMapping("/user") //user path로 진입 시 아래 함수 실행
+    @PostMapping("/user") //user path로 "진입 시" 아래 함수 실행
     public void saveUser(@RequestBody UserCreateRequest request) {
-        //TODO: process POST request
+        users.add(new User(request.getName(), request.getAge())); //POST/user 호출 시 arraylist에 추가
+        //해당 함수가 잘 마무리되면 200 OK 반환
     }
     
+    @GetMapping("/user")
+    public List<UserResponse> getUsers( ) {
+        List<UserResponse> responses = new ArrayList();
+        for (int i = 0 ; i < users.size() ; i++ )
+        {
+            responses.add(new UserResponse(i + 1, users.get(i)));
+        }
+
+        return responses;
+    }
+    
+
 }
--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -5,6 +5,22 @@ public class User {
     private String name;
     private Integer age;
 
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
     public User(String name, Integer age)
     {
         if (name == null || name.isBlank())
